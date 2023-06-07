8f886e29a7022144a6b0e8e1fa37c251a23424c2
R0 <68852309+R0-32@users.noreply.github.com>
2023-06-07 09:35:39 +0000

    ff

--- Program.cs
+++ Program.cs
@@ -1,34 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using Octokit;
-using Terminal.Gui;
 
-namespace terminote_bl
+namespace CodespacesBlankProgram
 {
+    class MenuItem
+    {
+        public int Number { get; set; }
+        public string Title { get; set; }
+    }
+
     class Program
     {
-        private static GitHubClient client;
-        private static string currentUsername;
-        private static TokenManager tokenManager;
+        static GitHubClient client;
+        static string currentUsername;
+        static TokenManager tokenManager;
 
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             client = new GitHubClient(new ProductHeaderValue("terminote"));
             currentUsername = "";
             tokenManager = new TokenManager();
 
-            // Выполняем авторизацию
-            await GhLogin();
+            bool exit = false;
+            int selectedOption = 0;
 
-            // Выводим список репозиториев
-            await ShowRepositories();
+            var options = new List<MenuItem>
+            {
+                new MenuItem { Number = 1, Title = "Вход в GitHub" },
+                new MenuItem { Number = 2, Title = "Просмотреть репозитории" },
+                new MenuItem { Number = 3, Title = "Закрыть приложение" }
+            };
 
-            Console.WriteLine("Нажмите любую клавишу, чтобы выйти...");
-            Console.ReadKey();
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Главное меню:");
+
+                for (int i = 0; i < options.Count; i++)
+                {
+                    if (i == selectedOption)
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                        Console.Write("-> ");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.Write("   ");
+                    }
+
+                    Console.WriteLine($"{options[i].Number}. {options[i].Title}");
+                }
+
+                var keyInfo = Console.ReadKey(true);
+
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        selectedOption = (selectedOption - 1 + options.Count) % options.Count;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        selectedOption = (selectedOption + 1) % options.Count;
+                        break;
+                    case ConsoleKey.Enter:
+                        if (options[selectedOption].Number == 1)
+                        {
+                            Login();
+                        }
+                        else if (options[selectedOption].Number == 2)
+                        {
+                            ShowRepositories();
+                        }
+                        else if (options[selectedOption].Number == 3)
+                        {
+                            exit = true;
+                        }
+                        break;
+                    case ConsoleKey.Escape:
+                        exit = true;
+                        break;
+                }
+
+            } while (!exit);
         }
 
-        public static async Task GhLogin()
+        static void Login()
         {
             Console.WriteLine("Подключение к GitHub");
 
@@ -36,12 +95,12 @@ namespace terminote_bl
             {
                 Console.WriteLine($"Вы уже подключены к аккаунту {currentUsername}");
                 Console.WriteLine("1. Ввести другой токен");
-                Console.WriteLine("2. Продолжить с текущим токеном");
+                Console.WriteLine("2. Назад");
 
                 var options = new List<MenuItem>
                 {
                     new MenuItem { Number = 1, Title = "Ввести другой токен" },
-                    new MenuItem { Number = 2, Title = "Продолжить с текущим токеном" }
+                    new MenuItem { Number = 2, Title = "Назад" }
                 };
 
                 var choice = GetArrowSelection(options);
@@ -49,6 +108,11 @@ namespace terminote_bl
                 if (choice.Number == 1)
                 {
                     currentUsername = "";
+                }
+                else if (choice.Number == 2)
+                {
+                    Console.Clear();
+                    return;
                 }
             }
 
@@ -61,16 +125,18 @@ namespace terminote_bl
 
             client.Credentials = new Credentials(accessToken);
 
-            if (await ValidateCredentials())
+            if (ValidateCredentials())
             {
                 try
                 {
-                    var user = await client.User.Current();
+                    var user = client.User.Current().Result;
 
                     currentUsername = user.Login;
 
                     Console.WriteLine("Успешная аутентификация!");
                     Console.WriteLine($"Добро пожаловать, {currentUsername}!");
+                    Console.WriteLine("Нажмите любую клавишу, чтобы продолжить...");
+                    Console.ReadKey();
                 }
                 catch (Exception ex)
                 {
@@ -80,14 +146,24 @@ namespace terminote_bl
             else
             {
                 Console.WriteLine("Ошибка: Неправильный токен доступа!");
+                Console.WriteLine("Нажмите любую клавишу, чтобы продолжить...");
+                Console.ReadKey();
             }
         }
 
-        public static async Task ShowRepositories()
+        static void ShowRepositories()
         {
+            if (string.IsNullOrEmpty(currentUsername))
+            {
+                Console.WriteLine("Ошибка: Вы не подключены к аккаунту GitHub!");
+                Console.WriteLine("Нажмите любую клавишу, чтобы продолжить...");
+                Console.ReadKey();
+                return;
+            }
+
             try
             {
-                var repositories = await client.Repository.GetAllForUser(currentUsername);
+                var repositories = client.Repository.GetAllForUser(currentUsername).Result;
 
                 Console.WriteLine($"Репозитории пользователя {currentUsername}:");
                 Console.WriteLine();
@@ -96,20 +172,26 @@ namespace terminote_bl
                 {
                     Console.WriteLine(repository.Name);
                 }
+
+                Console.WriteLine();
+                Console.WriteLine("Нажмите любую клавишу, чтобы продолжить...");
+                Console.ReadKey();
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Ошибка при получении репозиториев: {ex.Message}");
+                Console.WriteLine("Нажмите любую клавишу, чтобы продолжить...");
+                Console.ReadKey();
             }
         }
 
-        private static async Task<bool> ValidateCredentials()
+        static bool ValidateCredentials()
         {
             try
             {
                 if (string.IsNullOrEmpty(currentUsername))
                 {
-                    var user = await client.User.Current();
+                    var user = client.User.Current().Result;
                     currentUsername = user.Login;
                 }
 
@@ -122,7 +204,7 @@ namespace terminote_bl
             }
         }
 
-        private static MenuItem GetArrowSelection(List<MenuItem> options)
+        static MenuItem GetArrowSelection(List<MenuItem> options)
         {
             int selectedOption = 0;
             ConsoleKey key;
@@ -165,6 +247,63 @@ namespace terminote_bl
             } while (key != ConsoleKey.Enter);
 
             return options[selectedOption];
+        }
+    }
+
+    class TokenManager
+    {
+        public string GetAccessToken()
+        {
+            Console.Write("Токен доступа: ");
+            string accessToken = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(accessToken))
+            {
+                Console.WriteLine("Ошибка: Необходимо ввести токен!");
+                Console.WriteLine("Нажмите любую клавишу, чтобы продолжить...");
+                Console.ReadKey();
+                return null;
+            }
+
+            return accessToken;
+        }
+    }
+
+    class ShowRepositories
+    {
+        private static GitHubClient client;
+        private static string currentUsername;
+
+        public ShowRepositories(GitHubClient client, string currentUsername)
+        {
+            ShowRepositories.client = client;
+            ShowRepositories.currentUsername = currentUsername;
+        }
+
+        public void DisplayRepositories()
+        {
+            try
+            {
+                var repositories = client.Repository.GetAllForUser(currentUsername).Result;
+
+                Console.WriteLine($"Репозитории пользователя {currentUsername}:");
+                Console.WriteLine();
+
+                foreach (var repository in repositories)
+                {
+                    Console.WriteLine(repository.Name);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Нажмите любую клавишу, чтобы продолжить...");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при получении репозиториев: {ex.Message}");
+                Console.WriteLine("Нажмите любую клавишу, чтобы продолжить...");
+                Console.ReadKey();
+            }
         }
     }
 
